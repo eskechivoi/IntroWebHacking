10d2bc2eb761aef1a3b452c01639837a22dac88b
ferrodr <fernando.rguezm@gmail.com>
2023-09-06 19:56:50 +0200

    Finished configuring backend

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,26 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-
 const loginRouter = require('./controllers/login')
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose.connect(url)
+	.then(result => {
+	console.log('connected to MongoDB')
+	})
+	.catch((error) => {
+	console.log('error connecting to MongoDB:', error.message)
+	})
 
 app.use(express.json())
 app.use('/api/login', loginRouter)
 
-app.post('/api/register', (request, response) => {
-
-})
-
-app.post('/api/profile', (request, response) => {
-	
-})
-
-app.put('/api/changepassword', (request, response) => {
-
-})
-
-const PORT = 3001
+const PORT = process.env.PORT
 app.listen(PORT)
 console.log(`App listening on port ${PORT}`)
