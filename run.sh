fd42a1f03d82e24d3bc86e5ea1e544a6344137b2
ferrodr <fernando.rguezm@gmail.com>
2023-09-30 00:50:58 +0200

    Added install and run scripts

---
+++ run.sh
@@ -0,0 +1,14 @@
+# Make sure npm is installed
+sudo apt install npm
+
+# Install and serve backend
+cd ./backend
+npm install
+npm start &
+
+# Install and serve frontend
+cd ../frontend
+npm install
+sudo npm install -g serve
+npm run build
+sudo serve -s dist -l 80 &
