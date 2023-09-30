44f53b65977835045cf15759c3f83105aeb28563
ferrodr <fernando.rguezm@gmail.com>
2023-09-30 12:25:41 +0200

    Servidor del frontend

---
+++ frontend/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Sirve los archivos estáticos de la carpeta dist
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.listen(80, () => {
+  console.log('El servidor está corriendo en el puerto 80');
+});
