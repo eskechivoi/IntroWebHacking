20fed6e2050335d39b6b674e8a21ca5ad70d3e91
ferrodr <fernando.rguezm@gmail.com>
2023-09-30 16:40:06 +0200

    Redirects requests to frontend

--- backend/index.js
+++ backend/index.js
@@ -31,13 +31,27 @@ app.use('/api/profile', profileRouter)
 // Sirve los archivos estáticos de la carpeta dist
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-// Redirige todas las rutas al index.html
-app.get('/*', function(req, res) {
+function redirectFront (req, res) {ç
 	res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'), function(err) {
-	  if (err) {
-		res.status(500).send(err)
-	  }
-	})
+		if (err) {
+		  res.status(500).send(err)
+		}
+	  })
+}
+
+// Redirige las rutas del front al index.html
+app.get('/login', function(req, res) {
+	redirectFront(req,res)
+})
+
+// Redirige las rutas del front al index.html
+app.get('/register', function(req, res) {
+	redirectFront(req,res)
+})
+
+// Redirige las rutas del front al index.html
+app.get('/profile', function(req, res) {
+	redirectFront(req,res)
 })
 
 const PORT = process.env.PORT
