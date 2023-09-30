54c6e91034bf46eb71dcd629139fdc9993fb9309
ferrodr <86804978+eskechivoi@users.noreply.github.com>
2023-09-30 12:36:23 +0200

    Update server.js

--- frontend/server.js
+++ frontend/server.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+
 const app = express();
 
 // Sirve los archivos estáticos de la carpeta dist
