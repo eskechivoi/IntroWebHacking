d19eef101c251f568211eab0b5f1e26eb5efab09
ferrodr <fernando.rguezm@gmail.com>
2023-09-30 14:27:31 +0200

    Updated bootstrap location

--- frontend/src/header.jsx
+++ frontend/src/header.jsx
@@ -1,6 +1,6 @@
 function Header (props) {
     return (
-        <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossOrigin="anonymous"/>
+        <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css"/>
 )}
 
 export default Header;
